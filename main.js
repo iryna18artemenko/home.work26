75f954e31413c7455b04f826eaed9496d1e9ed33
iryna18artemenko <136636205+iryna18artemenko@users.noreply.github.com>
2023-08-29 12:44:31 +0300

    rework

---
+++ main.js
@@ -0,0 +1,17 @@
+const tbody = document.querySelector("tbody"); 
+
+for (let t =0; t < 10; t++) {
+
+   const tr = document.createElement("tr");
+
+    for (let i= 0; i < 10; i++ ) {
+
+        let td = document.createElement("td");
+        td.innerText =(t+1)*(i+1);
+        tr.append(td);
+
+
+    }
+
+    tbody.append(tr);
+}
